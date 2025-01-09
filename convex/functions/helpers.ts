dece6740118f942b481f9a30ab1bb85764fae2bd
nk0311 <navaitk031@gmail.com>
2025-01-08 20:07:24 -0500

    implemented authenticated query and mutation wrappers

---
+++ convex/functions/helpers.ts
@@ -0,0 +1,25 @@
+import { customCtx, customQuery } from "convex-helpers/server/customFunctions";
+import { getCurrentUser } from "./user";
+import { query } from "../_generated/server";
+
+export const authenticatedQuery = customQuery(
+  query,
+  customCtx(async (ctx) => {
+    const user = await getCurrentUser(ctx);
+    if (!user) {
+      throw new Error("Unauthorized");
+    }
+    return { user };
+  })
+);
+
+export const authenticatedMutation = customQuery(
+  query,
+  customCtx(async (ctx) => {
+    const user = await getCurrentUser(ctx);
+    if (!user) {
+      throw new Error("Unauthorized");
+    }
+    return { user };
+  })
+);
